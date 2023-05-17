cca7bfdd58dcd36c1c3876a33de6d8b3a692ad9b
leestana01 <leestana01@naver.com>
2023-05-17 22:55:25 +0900

    코드를 간소화하고 주석을 많이 달았습니다

--- bustimeline/DownBus/views.py
+++ bustimeline/DownBus/views.py
@@ -4,19 +4,39 @@ from .models import BusList, NowDay
 from datetime import datetime
 
 def busListView(request):
-    today = datetime.today().weekday()
     now_time = datetime.now().time()
+
+    day_type = checkDay() # 요일 반환
+
+    buses = BusList.objects.filter(day_type=day_type).order_by('bus_time')
+    # day_type(요일)에 해당하는 것들만 필터링해줍니다.
+    # 이후 버스 시간 순서대로 정렬합니다.
+    # 버스를 현 시간 이후것들만 보여주고 싶다면 필터에 ', bus_time__gte=now_time' 를 추가하세요
+    
+    formatted_time = now_time.strftime("%H시 %M분") # 기준시간 표시를 위한 문자열 포맷팅
+    return render(request, 'bus_list.html', {'buses': buses, 'now_time' : formatted_time})
+
+
+# 메인 페이지 잠깐 쓰려고 만들었습니다. 지우려면 지우세요.
+def mainPage(request):
+    return render(request, 'mainpage.html')
+
+
+
+
+# -----------------------------------------------------------------
+# 일반 함수 호출 영역입니다
+
+
+# 주중, 토요일, 일요일 체크하여 day_type을 반환하는 함수
+def checkDay():
+    today = datetime.today().weekday()
 
     if today < 5:  # 주중
         day_type = NowDay.objects.get(day_type='WEEK')
     elif today == 5:  # 토요일
         day_type = NowDay.objects.get(day_type='SAT')
     else:  # 일요일
-        day_type = NowDay.objects.get(day_type='SUN')
+        day_type = NowDay.objects.get(day_type='SUN')    
 
-    buses = BusList.objects.filter(day_type=day_type, bus_time__gte=now_time).order_by('bus_time')
-    formatted_time = now_time.strftime("%H시 %M분")
-    return render(request, 'bus_list.html', {'buses': buses, 'now_time' : formatted_time})
-
-def mainPage(request):
-    return render(request, 'mainpage.html')
+    return day_type
--- bustimeline/bustimeline/settings.py
+++ bustimeline/bustimeline/settings.py
@@ -11,10 +11,18 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from environ import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = Env()
+# env_path = BASE_DIR / ".env"
+# if env_path.exists():
+#     with env_path.open("rt", encoding="utf8") as f:
+#         env.read_env(f, overwrite=True)
+env.read_env()
+SERVICE_KEY = env('SERVICE_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
