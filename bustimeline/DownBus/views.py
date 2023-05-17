1eb6eb8e24588b53f54697a88868286f5d904bdd
leestana01 <leestana01@naver.com>
2023-05-17 10:52:57 +0900

    현재 시간 기준 이후 버스만 출력하도록 수정

--- bustimeline/DownBus/views.py
+++ bustimeline/DownBus/views.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 def busListView(request):
     today = datetime.today().weekday()
+    now_time = datetime.now().time()
 
     if today < 5:  # 주중
         day_type = NowDay.objects.get(day_type='WEEK')
@@ -13,8 +14,9 @@ def busListView(request):
     else:  # 일요일
         day_type = NowDay.objects.get(day_type='SUN')
 
-    buses = BusList.objects.filter(day_type=day_type)
-    return render(request, 'bus_list.html', {'buses': buses})
+    buses = BusList.objects.filter(day_type=day_type, bus_time__gte=now_time)
+    formatted_time = now_time.strftime("%H시 %M분")
+    return render(request, 'bus_list.html', {'buses': buses, 'now_time' : formatted_time})
 
 def mainPage(request):
     return render(request, 'mainpage.html')
