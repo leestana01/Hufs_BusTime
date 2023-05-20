a7be061d88878c8a7fff77a8d974f5f17600f0e4
nayeong lee <lny021102@gmail.com>
2023-05-21 03:52:54 +0900

    쓸데없는 JSon 부분 삭제, apicaller에서 location만 가져와서 띄우는  새 코드 추가(동작이 안되니 확인 필요), bus DB에 따라 시간표 옆에 어떻게 띄울건지 갈피를 잡음(미완성->내일)

--- bustimeline/UpBus/views.py
+++ bustimeline/UpBus/views.py
@@ -3,46 +3,50 @@ from django.shortcuts import render
 import urllib.parse
 from .apicaller import APICaller
 
+#수정!! 확인바람
 
 def bus_list(request):
-    
-    # stationId= [29554, 29555, 47061, 47060, 29560, 47606, 29561]       # 수혁 : 이건 정류장id임. stationId 써야함.
+
     # 버스 번호와 정류장 정보
-    bus_numbers1 = [1303, 1117, 1150, 1005]
-    bus_numbers2 = [1303, 1117, 1150, 1005]
+    bus_numbers = [1303, 1117, 1150, 1005]
     bus_stops = ['외대사거리', '모현사거리', '기숙사', '도서관', '파란지붕']
 
     # 실시간 버스 위치 정보를 저장할 딕셔너리
     bus_locations_Up = APICaller(228000349) #상행 종점 | 한국외대종점
     bus_locations_Down = APICaller(228000346) #하행 종점 | 외대.모현빌라
     bus_locations = [bus_locations_Down, bus_locations_Up]
-            
+    
+    # 버스 위치 정보를 담을 딕셔너리 초기화
+    bus_locations_info = {}
+    
+    #api caller에서 location만 받아오기
+    location_1303 = bus_locations_Down.get("1303", {}).get("location")
+    location_1117 = bus_locations_Down.get("1117", {}).get("location")
+    location_1150 = bus_locations_Down.get("1150", {}).get("location")
+    location_1005 = bus_locations_Down.get("1005", {}).get("location")
+    
+    location_1303 = bus_locations_Up.get("1303", {}).get("location")
+    location_1117 = bus_locations_Up.get("1117", {}).get("location")
+    location_1150 = bus_locations_Up.get("1150", {}).get("location")
+    location_1005 = bus_locations_Up.get("1005", {}).get("location")
 
-    bus_number_ranges = {
-        bus_number: list(range(bus_number + 1, bus_number + 5))
-        for bus_number in bus_numbers1 + bus_numbers2
-    }
+
+    # 버스 위치 정보 추가
+    for bus_location in bus_locations:
+        for bus_number, location_info in bus_location.items():
+            if 'location' in location_info and location_info['location'] != 'N/A':
+                location = int(location_info['location'])  # 현재 위치를 정수로 변환
+                if location > 0:  # 현재 위치가 양수인 경우에만 정류장에 표시
+                    bus_locations_info[str(bus_number)] = str(bus_stops[location-1])
 
     context = {
-        'bus_numbers1': bus_numbers1,
-        'bus_numbers2': bus_numbers2,
-        'bus_locations': bus_locations,
+        'bus_numbers': bus_numbers,
         'bus_stops': bus_stops,
-        'bus_number_ranges': bus_number_ranges,
+        'bus_locations_info': bus_locations_info,
+        'location_1303': location_1303,
+        'location_1117': location_1117,
+        'location_1150': location_1150,
+        'location_1005': location_1005,
     }
 
     return render(request, 'bus_list.html', context)
-
-def parse_bus_location(response_text):
-    bus_location = 'N/A'
-
-    if response_text:
-        root = ET.fromstring(response_text)
-        item_nodes = root.findall('.//itemList')
-        for item_node in item_nodes:
-            bus_location_node = item_node.find('locationNo1')
-
-            if bus_location_node is not None:
-                bus_location = bus_location_node.text
-
-    return bus_location, None
