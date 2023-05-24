1ffcc3dcc9639029e1f92e2376a179320fc78ca0
leestana01 <leestana01@naver.com>
2023-05-24 18:55:40 +0900

    템플릿 Margin 요소 전부 제거 후 대체, 모바일 최적화 UI 제작

--- bustimeline/bustimeline/settings.py
+++ bustimeline/bustimeline/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-7=n!y#gkyb2@r_0n#3@-47_rhw&!&gh!!d==igeh=5(@db9j@5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
