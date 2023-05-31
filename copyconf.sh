a92cbcf13e6d72f706edfde96d897c8db66cc699
leestana01 <leestana01@naver.com>
2023-05-31 23:10:32 +0900

    불필요 파일 제거

--- copyconf.sh
+++
@@ -1,3 +0,0 @@
-sudo cp -f /srv/Hufs_BusTime/bustimeline/.config/nginx/bustimeline.conf /etc/nginx/sites-available/bustimeline.conf
-
-sudo ln -sf /etc/nginx/sites-available/bustimeline.conf /etc/nginx/sites-enabled/bustimeline.conf
